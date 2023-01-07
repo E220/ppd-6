9d5cfc5d7d51fca73c189a544829c8ba38d4efb9
Tiha <tgtihamer@yahoo.com>
2023-01-07 12:59:33 +0200

    add thread pool Strategy

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,36 @@
 import algorithm.HamiltonianCycle;
 import model.Graph;
 import strategy.WithSingleThread;
+import strategy.WithThreadPool;
 
 public class Main {
     public static void main(String[] args) {
-        final Graph withoutCycle = new Graph(7);
-        withoutCycle.addEdge(0, 1);
-        withoutCycle.addEdge(0, 3);
-        withoutCycle.addEdge(0, 4);
-        withoutCycle.addEdge(1, 2);
-        withoutCycle.addEdge(1, 6);
-        withoutCycle.addEdge(2, 1);
-        withoutCycle.addEdge(2, 5);
-        withoutCycle.addEdge(2, 6);
-//        withoutCycle.addEdge(3, 0);
-        withoutCycle.addEdge(3, 6);
-        withoutCycle.addEdge(4, 3);
-        withoutCycle.addEdge(4, 5);
-        withoutCycle.addEdge(5, 0);
-        withoutCycle.addEdge(5, 1);
-        withoutCycle.addEdge(5, 4);
-        withoutCycle.addEdge(5, 3);
-        withoutCycle.addEdge(6, 4);
+        final Graph graph = new Graph(7);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 3);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 6);
+        graph.addEdge(2, 1);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+        graph.addEdge(3, 0);
+        graph.addEdge(3, 6);
+        graph.addEdge(4, 3);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 0);
+        graph.addEdge(5, 1);
+        graph.addEdge(5, 4);
+        graph.addEdge(5, 3);
+        graph.addEdge(6, 4);
 
-        System.out.println(HamiltonianCycle.find(withoutCycle, new WithSingleThread()));
-//        for (int i = 0; i < 7; i++)
-//            System.out.println(algorithm.HamiltonianCycle.find(withoutCycle, i, new WithSingleThread()));
+//        System.out.println(HamiltonianCycle.find(graph, new WithSingleThread()));
+        for (int i = 0; i < 7; i++)
+            System.out.println(HamiltonianCycle.find(graph, i, new WithSingleThread()));
+
+//        System.out.println(HamiltonianCycle.find(graph, new WithThreadPool()));
+        for (int i = 0; i < 7; i++) {
+            System.out.println(HamiltonianCycle.find(graph, i, new WithThreadPool()));
+        }
     }
 }
--- src/strategy/WithThreadPool.java
+++ src/strategy/WithThreadPool.java
@@ -1,4 +1,55 @@
 package strategy;
 
-public class WithThreadPool {
+import algorithm.State;
+import model.Graph;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class WithThreadPool implements Strategy {
+
+    private final ReentrantReadWriteLock resultLock = new ReentrantReadWriteLock();
+    private LinkedList<Integer> result;
+
+    @Override
+    public LinkedList<Integer> find(Graph graph, State state) {
+        final List<Integer> neighbors = graph.getNeighbors(state.path().getLast());
+        try (final ExecutorService pool = Executors.newFixedThreadPool(neighbors.size())) {
+            for (int next : neighbors) {
+                if (next == state.path().getFirst() && state.path().size() == graph.getNrNodes()) {
+                    return state.path();
+                }
+                if (!state.isSelected()[next]) {
+                    if (this.noSolutionSoFar()) {
+                        pool.execute(() -> {
+                            final LinkedList<Integer> result = find(graph, state.with(next));
+                            if (Objects.nonNull(result) && this.noSolutionSoFar()) {
+                                this.solutionFound(result);
+                            }
+                        });
+                    } else {
+                        return this.result;
+                    }
+                }
+            }
+        }
+        return this.result;
+    }
+
+    private void solutionFound(LinkedList<Integer> result) {
+        this.resultLock.writeLock().lock();
+        this.result = result;
+        this.resultLock.writeLock().unlock();
+    }
+
+    private boolean noSolutionSoFar() {
+        this.resultLock.readLock().lock();
+        final boolean noSolution = Objects.isNull(this.result);
+        this.resultLock.readLock().unlock();
+        return noSolution;
+    }
 }
